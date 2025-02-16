9e3cd2e4e4603952b28d7b1257bb49b6ab179a38
Elfground <elfground@gmail.com>
2025-02-16 13:05:45 +0100

    Added icons and logo and removed React logos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import logo from "./favicon-dictionary.png"
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-    Here's the Dictionary
-    </div>
+      <header>
+      <h1>World of Words Dictionary</h1>
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        
+    </header>
     <footer>
       Made by <a href="https://github.com/Elfground" rel="noreferrer">Monica Alvmarken</a>, open-sourced on <a href="https://github.com/Elfground/react-dictionary-app" rel="noreferrer">GitHub</a>
     </footer>
