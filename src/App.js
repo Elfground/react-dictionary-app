09fdced0cc0b050082fc64d0baebc37903b9ac72
Elfground <elfground@gmail.com>
2025-02-16 12:39:26 +0100

    added link and installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
     Here's the Dictionary
+    </div>
+    <footer>
+      Made by <a href="https://github.com/Elfground">Monica Alvmarken</a>, open-sourced on GitHub
+    </footer>
     </div>
   );
 }
